34b334afeb4639f6680bb4205f1192f41fb6dc40
Antonio Yang <yanganto@gmail.com>
2021-06-04 11:19:28 +0800

    Disable tracing subscriber

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,27 @@ fn main() {
 }
 
 fn init_tracing() {
-    use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
+    // FIXME: there is an issue on setting the logger
+    // `Err` value: SetLoggerError(())'
+    //
+    // use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
-    let base_subscriber = tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .with_target(true);
-    if cfg!(not(debug_assertions)) {
-        base_subscriber.json().with_ansi(false).init();
-    } else {
-        let subscriber = base_subscriber
-            .with_span_events(FmtSpan::CLOSE)
-            .without_time()
-            .pretty()
-            .with_test_writer();
-        if std::env::var("VERBOSE_TRACING").unwrap_or_default() != "0" {
-            subscriber.try_init().ok();
-        } else {
-            subscriber.compact().try_init().ok();
-        }
-    }
+    // let base_subscriber = tracing_subscriber::fmt()
+    //     .with_env_filter(EnvFilter::from_default_env())
+    //     .with_target(true);
+
+    // if cfg!(not(debug_assertions)) {
+    //     base_subscriber.json().with_ansi(false).init();
+    // } else {
+    //     let subscriber = base_subscriber
+    //         .with_span_events(FmtSpan::CLOSE)
+    //         .without_time()
+    //         .pretty()
+    //         .with_test_writer();
+    //     if std::env::var("VERBOSE_TRACING").unwrap_or_default() != "0" {
+    //         subscriber.try_init().ok();
+    //     } else {
+    //         subscriber.compact().try_init().ok();
+    //     }
+    // }
 }
