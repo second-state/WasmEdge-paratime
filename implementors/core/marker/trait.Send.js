255b368b7ae4bd8bc805c16cf2f49ec84a2c9daf
yanganto <yanganto@users.noreply.github.com>
2021-06-04 05:16:19 +0000

    Automated deployment: Fri Jun  4 05:16:19 UTC 2021 34b334afeb4639f6680bb4205f1192f41fb6dc40

--- implementors/core/marker/trait.Send.js
+++ implementors/core/marker/trait.Send.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["wasmedge_paratime"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wasmedge_paratime/struct.Runtime.html\" title=\"struct wasmedge_paratime::Runtime\">Runtime</a>","synthetic":true,"types":["wasmedge_paratime::Runtime"]}];
+implementors["wasmedge_paratime"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"wasmedge_paratime/struct.Runtime.html\" title=\"struct wasmedge_paratime::Runtime\">Runtime</a>","synthetic":true,"types":["wasmedge_paratime::runtime::Runtime"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["wasmedge_paratime"] = {"name":"","dirs":[{"name":"module","files":["auth.rs","block.rs","methods.rs","migrate.rs","mod.rs"]}],"files":["account.rs","lib.rs","vm.rs"]};
+sourcesIndex["wasmedge_paratime"] = {"name":"","dirs":[{"name":"module","files":["auth.rs","block.rs","methods.rs","migrate.rs","mod.rs"]}],"files":["account.rs","lib.rs","runtime.rs","vm.rs"]};
 createSourceSidebar();
